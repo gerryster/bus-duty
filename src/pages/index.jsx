2fe7bf8c820c1b380d7b3de3b6d3f1c83b662189
Ryan Gerry <gerryster@gmail.com>
2014-11-23 17:17:05 -0600

    Let's add some buses!

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,14 +11,36 @@ var HomePage = React.createClass({
   getDefaultProps() {
     return {
       title: 'Bus Duty',
-      layout: DefaultLayout
+      layout: DefaultLayout,
+      buses: [
+        { number: 80,
+          numKids: 2,
+        },
+        { number: 82,
+          numKids: 5,
+        },
+        { number: 83,
+          numKids: 2,
+        },
+        { number: 84,
+          numKids: 2,
+        },
+        { number: 85,
+          numKids: 2,
+        },
+      ]
     };
   },
   render() {
     return (
-      <div className="container">
+      <div id="content" className="container">
         <div className="row">
           <div className="col-sm-4">
+            <ul>
+              {this.props.buses.map(function(bus){
+                return <li>{bus.number}</li>
+              })}
+            </ul>
           </div>
         </div>
       </div>
